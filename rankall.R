5966c454fde86dfea9760b69990bdac4a8bcf983
Manuel Martínez <mmblanes@drimay.es>
2016-03-19 19:02:38 +0100

    Added files via upload

---
+++ rankall.R
@@ -0,0 +1,42 @@
+rankall <- function(outcome, num = "best") {
+  ## Read outcome data
+  
+  x <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
+  
+  ## Check that state and outcome are valid
+  
+    icol <- if (outcome == "heart attack") {
+    11
+  } else if (outcome == "heart failure") {
+    17
+  } else if (outcome == "pneumonia") {
+    23
+  } else {
+    stop("invalid outcome")
+  }
+  
+  ## For each state, find the hospital of the given rank
+    
+  x <- x[, c(7, 2, icol)]
+  x[, 3] <- suppressWarnings(as.numeric(x[, 3]))
+  x <- x[order(x[1], x[3], x[2]), ]
+  x <- x[!is.na(x[3]), ]
+  y <- split(x, x[1])
+  
+  if (num == "best") {
+    z <- lapply(y, function(i) { i[1,] })
+  } else if (num == "worst") {
+    z <- lapply(y, function(i) { i[nrow(i),] })
+  } else {
+    z <- lapply(y, function(i) { i[num,] })
+  }
+  
+  ## Return a data frame with the hospital names and the
+  ## (abbreviated) state name
+  
+  res <- as.data.frame(do.call(rbind, z))
+  res[,1] <- row.names(res)
+  res <- res[, 2:1]
+  colnames(res) <- c("hospital", "state")
+  res
+}
